256962c0aeed8246da42c89d434b1bcf43dc1f48
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2023-09-07 22:57:02 -0700

    Add single-variable version

--- beta.py
+++ beta.py
@@ -1,4 +1,4 @@
-from numpy import sin, cos, pi, sqrt
+from numpy import sin, cos, pi, arctan, sqrt
 import scipy
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,51 +6,39 @@ import matplotlib.animation as animation
 from collections import deque
 
 l = 1.0  # initial length of spring in m
-φ = 1.0 # initial winding angle of spring in rad
+φ = 20.0 # initial winding angle of spring in rad
 r = 0.1  # radius of spring in m
 R = 0.3 # radius of ring
 
-k = 10.0 # linear spring constant in N/m
+k = 15.0 # linear spring constant in N/m
 κ = 10.0 # torsional spring constant in N*m/rad
 
-M = 1.0  # mass of ring in kg
-I = M*R*R # moment of inertia of ring in kg*m^2
+m = 1.0  # mass of ring in kg
+I = m*R*R # moment of inertia of ring in kg*m^2
 
 t_stop = 10  # how many seconds to simulate
 history_len = 1000  # how many trajectory points to display
 
 γ = 1.0 # coupling factor; should be 1
 
-X, V, Θ, Ω = 0,1,2,3
-
-x0 = -0.04*l # initial vertical displacement in m
-v0 = 0.02*l # initial vertical velocity in m/s
-θ0 = -0.05*φ # initial anglular displacement in rad
-ω0 = 0.02*φ # initial angular velocity in rad/s
+d = sqrt(l*l + r*r*φ*φ)
+b0 = arctan(l/r/φ)
+bb0 = 0.3
 
 # initial state
-state = [x0, v0, θ0, ω0]
+state = [b0, bb0]
 
 def derivs(_, state):
     ddt = np.zeros_like(state)
 
-    x = state[X]
-    v = state[V]
-    θ = state[Θ]
-    ω = state[Ω]
+    b = state[0]
+    bb = state[1]
 
-    a = M*I*v*v - I*k*x*(l+x) + M*I*r*r*ω*ω - M*r*r*κ*θ*(φ+θ)
-    b = I*(l+x)*(l+x) + M*r*r*r*r*(φ+θ)*(φ+θ)
-    λ = γ*a/b
+    numer = -m*bb*bb*sin(2*b) - I*bb*bb*sin(2*b)/r/r + 2*k*(sin(b) - sin(b0))*cos(b) - 2*k*(cos(b)-cos(b0))*sin(b)/r/r
+    denom = -(2*m*cos(b)*cos(b) + 2*I*sin(b)*sin(b)/r/r)
 
-    #print(v*(l+x) + ω*r*r*(φ+θ))
-    print(v)
-
-    ddt[X] = v
-    ddt[V] = -(k*x + λ*(l+x))/M
-
-    ddt[Θ] = ω
-    ddt[Ω] = -(κ*θ + λ*r*r*(φ+θ))/I
+    ddt[0] = bb
+    ddt[1] = numer/denom
 
     return ddt
 
@@ -60,17 +48,15 @@ speed = 10
 t = np.arange(0, t_stop, dt)
 
 # integrate the ODE using Euler's method
-y = np.empty((len(t), 4))
+y = np.empty((len(t), 2))
 y[0] = state
 solve = scipy.integrate.solve_ivp(derivs, t[[0, -1]], state, t_eval=t)
 
 print(solve)
 y = solve.y.T
 
-array_x = y[:, X]
-array_v = y[:, V]
-array_θ = y[:, Θ]
-array_ω = y[:, Ω]
+array_b = y[:, 0]
+array_bb = y[:, 1]
 
 fig, (delta_axes, net_axes, energy_axes) = plt.subplots(1, 3)
 
@@ -90,7 +76,7 @@ net_axes.grid()
 
 energy_axes.set_ylim(0, t_stop)
 energy_axes.set_ylabel("Time (s)")
-energy_axes.set_xlim(0, (k*x0*x0 + κ*θ0*θ0 + M*v0*v0 + I*ω0*ω0))
+energy_axes.set_xlim(0, 3)
 energy_axes.set_xlabel("Energy (J)")
 energy_axes.invert_yaxis()
 energy_axes.grid()
@@ -120,15 +106,18 @@ def animate(i):
     j = i*speed
     t = j*dt
 
-    x = array_x[j]
-    v = array_v[j]
-    θ = array_θ[j]
-    ω = array_ω[j]
+    b = array_b[j]
+    bb = array_bb[j]
 
-    Ex = 0#k*x*x/2
-    Ev = M*v*v/2
-    Eθ = 0#κ*θ*θ/2
-    Eω = 0#I*ω*ω/2
+    x = d*(sin(b) - sin(b0))
+    v = d*bb*cos(b)
+    θ = d*(cos(b) - cos(b0))/r
+    ω = d*bb*sin(b)/r
+
+    Ex = k*x*x/2
+    Ev = m*v*v/2
+    Eθ = κ*θ*θ/2
+    Eω = I*ω*ω/2
 
     if i == 0:
         history_t.clear()
@@ -156,7 +145,7 @@ def animate(i):
 
     N_arcs = 500
     net_line.set_data(
-        [r*sin((φ+θ)*n/5) for n in range(N_arcs+1)], 
+        [r*sin((φ+θ)*n/N_arcs) for n in range(N_arcs+1)], 
         [(n/N_arcs) * (l+x) for n in range(N_arcs+1)]
     )
     N_masses = 17
